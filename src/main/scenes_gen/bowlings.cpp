38092020ad8b81bcf643b1e8c8cd56779ef90d82
equation314 <equation618@gmail.com>
2017-06-23 05:36:58 +0800

    Modify bowling scene

--- src/main/scenes_gen/bowlings.cpp
+++ src/main/scenes_gen/bowlings.cpp
@@ -10,44 +10,56 @@
 
 int main()
 {
-    Config::depth_of_field_samples = 16;
-    Config::anti_aliasing_samples = 2;
+    Scene* scene = Scene::loadFrom("../scenes/bowlings/cornell_house.json");
+
+    Config::depth_of_field_samples = 4;
+    Config::anti_aliasing_samples = 0;
     Config::anti_aliasing_edge_detection_mode = 0;
     Config::ppm_photon_emitted_number = 10000;
-    Config::ppm_initial_search_radius = 0.5;
-    Config::ppm_photon_emitted_number = 1000000;
+    Config::ppm_initial_search_radius = 0.02;
+    Config::ppm_photon_emitted_number = 100000;
     Config::ppm_iteration_depth = 1000;
     Config::thread_max_number = 4;
-    Config::thread_max_number = 4;
 
-    Scene* scene = new Scene(new Camera(Vector3(-5, -6, 6), Vector3(-2, 17, 0), Vector3(0, 0, 1), 300, 480, 40, 0.25, 50), Color(0.0, 0.0, 0.0));
-    Material* glass = new Material(Color(1, 1, 1), 0, 0, 0.2, 0.8, 1.5, Color(0.5, 0.5, 0));
+    Material* glass = new Material(Color(0.7, 0.7, 0.7), 0.8, 0.2);
 
-    Material* m_ground = new Material(Color(1, 1, 1), 0.6, 0.1, 0.3);
-    m_ground->setTexture(new Bmp("../textures/floor.bmp"));
+    RotationBody* l = static_cast<RotationBody*>(Object::loadFrom("../scenes/bowlings/lamp.json"));
+    l->setO(Vector3(0, -0.5, 1.2));
+    scene->addObject(l);
+    scene->addObject(new Cylinder(Vector3(0, -0.5, 1.34), 0.005, 1, glass));
 
-    Plane* ground = new Plane(Vector3(0, 0, 1), 1, m_ground);
-    ground->setTextureAxis(Vector3(0, 0, 0), Vector3(15, 0, 0), Vector3(0, 15, 0));
+    l = static_cast<RotationBody*>(Object::loadFrom("../scenes/bowlings/lamp.json"));
+    l->setO(Vector3(-0.5, 0.5, 1));
+    scene->addObject(l);
+    scene->addObject(new Cylinder(Vector3(-0.5, 0.5, 1.14), 0.005, 1, glass));
 
-    scene->addLight(new PointLight(Color(1, 1, 1), Vector3(15, 5, 10), 13000));
-    scene->addLight(new PointLight(Color(1, 1, 1), Vector3(-15, 5, 10), 13000));
-    scene->addObject(ground);
+    l = static_cast<RotationBody*>(Object::loadFrom("../scenes/bowlings/lamp.json"));
+    l->setO(Vector3(0.5, 0.5, 1));
+    scene->addObject(l);
+    scene->addObject(new Cylinder(Vector3(0.5, 0.5, 1.14), 0.005, 1, glass));
 
-    Sphere* sphere = new Sphere(Vector3(-1, 10, 1), 2, glass);
-    scene->addObject(sphere);
+    l = static_cast<RotationBody*>(Object::loadFrom("../scenes/bowlings/lamp.json"));
+    l->setO(Vector3(-0.5, -1.5, 1));
+    scene->addObject(l);
+    scene->addObject(new Cylinder(Vector3(-0.5, -1.5, 1.14), 0.005, 1, glass));
 
-    const double r = 3;
+    l = static_cast<RotationBody*>(Object::loadFrom("../scenes/bowlings/lamp.json"));
+    l->setO(Vector3(0.5, -1.5, 1));
+    scene->addObject(l);
+    scene->addObject(new Cylinder(Vector3(0.5, -1.5, 1.14), 0.005, 1, glass));
+
+    const double r = 0.18;
     const int n = 4;
     double x = 0, y = -r * sqrt(3);
 
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n - i; j++)
         {
-            y = 20 * r + -(i * 2 * r - (n - 1) * r + 5 * r);
-            x = i * r + j * 2 * r - 2 * r;
+            y = 1 - (i * 2 * r - (n - 1) * r + 5 * r);
+            x = i * r + j * 2 * r - 3 * r;
 
             RotationBody* b = static_cast<RotationBody*>(Object::loadFrom("../scenes/bowlings/bowling.json"));
-            b->setO(Vector3(x, y, -1));
+            b->setO(Vector3(x, y, 0));
 
             scene->addObject(b);
         }
