10ae0edfb18c70dbaebf0deafbefe41f94a3e489
equation314 <equation618@gmail.com>
2017-12-08 21:54:43 +0800

    Fix some bugs on macOS

--- src/common/const.h
+++ src/common/const.h
@@ -11,32 +11,32 @@ namespace Const
 constexpr int HASH_BASE = 31415927;
 
 constexpr double EPS = 1e-6;
-constexpr double PI = acos(-1.0);
+constexpr double PI = M_PI;
 
 inline int randUInt()
 {
-#ifdef __linux
-    return rand();
-#else
+#ifdef _WIN32
     return (rand() << 15) | rand();
+#else
+    return rand();
 #endif
 }
 
 inline uint64 randUInt64()
 {
-#ifdef __linux
-    return (rand() << 31) | rand();
+#ifdef _WIN32
+    return (((((1ll * rand() << 15) | rand()) << 15) | rand()) << 15) | rand();
 #else
-    return (((((rand() << 15) | rand()) << 15) | rand()) << 15) | rand();
+    return (1ll * rand() << 31) | rand();
 #endif
 }
 
 inline double randDouble()
 {
-#ifdef __linux
-    return 1.0 * rand() / RAND_MAX;
-#else
+#ifdef _WIN32
     return 1.0 * randUInt() / (1 << 20);
+#else
+    return 1.0 * rand() / RAND_MAX;
 #endif
 }
 }
